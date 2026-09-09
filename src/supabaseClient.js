cb08e12459c3ceb4e594916d46c073d4e8590ceb
Majidul <majidulislam80999@gmail.com>
2026-09-09 18:50:26 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,4 +3,13 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://xeoxyqhqbcgpxrtcvepg.supabase.co'
 const supabaseAnonKey = 'sb_publishable_9Nf-byu-HPfY2H0q7LHAxQ_xHKJpy4L'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: false
+  },
+  global: {
+    fetch: (...args) => fetch(...args)
+  }
+})

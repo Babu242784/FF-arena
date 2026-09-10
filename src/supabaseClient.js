fff1323299dfc0fdeeba2e4eace928e431b4d7bb
Majidul <majidulislam80999@gmail.com>
2026-09-10 09:20:20 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,15 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://xeoxyqhqbcgpxrtcvepg.supabase.co'
-const supabaseAnonKey = 'sb_publishable_9Nf-byu-HPfY2H0q7LHAxQ_xHKJpy4L'
+const supabaseUrl = 'https://swoyqnqbcgpxrtcvwpyg.supabase.co'
+const supabaseAnonKey = 'sb_publishable_9Nf-byu-9PfY2H0q7LHxQ_xHKJpy4L'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
     detectSessionInUrl: false
-  },
-  global: {
-    headers: { 'x-my-custom-header': 'ff-arena' }
   }
 })

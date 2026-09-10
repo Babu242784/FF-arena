34ade6ed2ded873f4af7a3613315bd1d2e4d240c
Majidul <majidulislam80999@gmail.com>
2026-09-10 18:35:35 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xecwyehgbcgguxicvapg.supabase.co'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://xecwyehqbcgguxicvapg.supabase.co'
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlY3d5ZWhxYmNnZ3V4aWN2YXBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg0MjE4NDMsImV4cCI6MjEwMzk5Nzg0M30.yAk_hGId69tzNdTyBnXP5acrClvgXIC1lOQgQInmUqQ'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
